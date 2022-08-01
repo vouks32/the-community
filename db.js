4774366ce79c6aa2c2114f0b69c6ce9804674fa4
vouks32 <85527577+vouks32@users.noreply.github.com>
2022-08-01 16:35:33 +0100

    Update db.js

--- db.js
+++ db.js
@@ -16,7 +16,14 @@ var connection = mysql.createConnection({
 
 
 exports.con = function(){
-	connection.connect();
+	connection.connect(function(err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+
+  console.log('connected as id ' + connection.threadId);
+});
 }
 
 exports.select = function (query, values=null) {
